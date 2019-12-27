268d81bb528b23748c45af363f41ea2af7a0289a
onyx <onyx@localhost>
2019-12-27 13:17:59 +0200

    first pipeline file

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ remote_hosted=
 if [ "$remote_hosted" = "y" -o "$remote_hosted" = "Y" ]; then
       echo "Variable is NOT empty"
 else
-      echo "Variable is empty"
+      echo "Variable is empty" 
 fi
 
